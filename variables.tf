8a390f87f9bf0c803f3b2fe4d86ffe2a12ee26ae
ddimri <deepak_dimri@hotmail.com>
2018-01-05 06:15:56 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {}
+
+variable "vpc_name" {}
+
+variable "feyeeng_cidr_block" {}
+
+variable "vpc_cidr_block" {}
+
+variable "base_ami" {}
+
+variable "private_key" {}
+
+variable "public_subnet_cidr_block" {}
+
+variable "opendj_server_name" {}
+
+variable "instance_type" {}
+
+variable "aws_profile" {}
+
+variable "home_dir" {}
+
+variable "key_name" {}
+
+variable "ansible-playbook" {}
+
+variable "availability_zones" {
+  type        = "list"
+}
