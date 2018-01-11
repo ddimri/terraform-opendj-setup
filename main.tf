35fa4068f27236f067ae147b1fb32f8c7ed51766
ddimri <deepak_dimri@hotmail.com>
2018-01-11 20:09:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,10 +110,10 @@ resource "aws_eip" "opendj-source-ami-eip" {
   }
 }
 
-data "template_file" "run-ansible" {
+data "template_file" "run-ansible-playbooks" {
   template =  <<-EOF
               #!/bin/bash
               ansible-playbook /home/ubuntu/${var.copy_password_file} && ansible-playbook /home/ubuntu/${var.ansible_playbook}
               EOF
-  depends_on = ["file.opendj-copy-password.yml"]
+  #depends_on = ["file.opendj-copy-password.yml"]
 }
